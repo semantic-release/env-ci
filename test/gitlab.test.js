acd3f54b782de5cd0576a0e6a73fd0d2e6dc5c31
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-01-27 02:47:20 -0500

    fix: use CI_PROJECT_PATH for GitLab slug

--- test/gitlab.test.js
+++ test/gitlab.test.js
@@ -7,7 +7,7 @@ test('Push', t => {
   process.env.CI_JOB_NAME = '91011';
   process.env.CI_JOB_STAGE = '1234';
   process.env.CI_COMMIT_REF_NAME = 'master';
-  process.env.CI_REPOSITORY_URL = 'https://gitlab.com/owner/repo.git';
+  process.env.CI_PROJECT_PATH = 'owner/repo';
   process.env.CI_PROJECT_DIR = '/';
 
   t.deepEqual(gitlab.configuration(), {
@@ -18,25 +18,5 @@ test('Push', t => {
     root: '/',
     job: '1234',
     slug: 'owner/repo',
-  });
-});
-
-test('Push (no repo url)', t => {
-  process.env.GITLAB_CI = 'true';
-  process.env.CI_COMMIT_SHA = '5678';
-  process.env.CI_JOB_NAME = '91011';
-  process.env.CI_JOB_STAGE = '1234';
-  process.env.CI_COMMIT_REF_NAME = 'master';
-  process.env.CI_PROJECT_DIR = '/';
-  delete process.env.CI_REPOSITORY_URL;
-
-  t.deepEqual(gitlab.configuration(), {
-    service: 'gitlab',
-    commit: '5678',
-    build: '91011',
-    branch: 'master',
-    root: '/',
-    job: '1234',
-    slug: undefined,
   });
 });
