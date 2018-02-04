bedc9759c7cf037fd4732aff1a2c695c71ce72bc
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-02-03 23:38:38 -0500

    feat: add `buildUrl` and `jobUrl` properties

--- test/codeship.test.js
+++ test/codeship.test.js
@@ -4,6 +4,7 @@ import codeship from '../lib/codeship';
 test('Push', t => {
   process.env.CI_NAME = 'codeship';
   process.env.CI_BUILD_NUMBER = '91011';
+  process.env.CI_BUILD_URL = 'https://server.com/buildresult';
   process.env.CI_COMMIT_ID = '5678';
   process.env.CI_BRANCH = 'master';
   process.env.CI_REPO_NAME = 'owner/repo';
@@ -12,6 +13,7 @@ test('Push', t => {
     service: 'codeship',
     commit: '5678',
     build: '91011',
+    buildUrl: 'https://server.com/buildresult',
     branch: 'master',
     slug: 'owner/repo',
   });
--- test/gitlab.test.js
+++ test/gitlab.test.js
@@ -4,8 +4,11 @@ import gitlab from '../lib/gitlab';
 test('Push', t => {
   process.env.GITLAB_CI = 'true';
   process.env.CI_COMMIT_SHA = '5678';
-  process.env.CI_JOB_NAME = '91011';
+  process.env.CI_JOB_NAME = 'job_name';
   process.env.CI_JOB_STAGE = '1234';
+  process.env.CI_PIPELINE_ID = '91011';
+  process.env.CI_JOB_ID = '1213';
+  process.env.CI_PROJECT_URL = 'https://gitlab.com/owner/repo';
   process.env.CI_COMMIT_REF_NAME = 'master';
   process.env.CI_PROJECT_PATH = 'owner/repo';
   process.env.CI_PROJECT_DIR = '/';
@@ -13,10 +16,12 @@ test('Push', t => {
   t.deepEqual(gitlab.configuration(), {
     service: 'gitlab',
     commit: '5678',
-    build: '91011',
+    build: 'job_name',
+    buildUrl: 'https://gitlab.com/owner/repo/pipelines/91011',
     branch: 'master',
     root: '/',
     job: '1234',
+    jobUrl: 'https://gitlab.com/owner/repo/-/jobs/1213',
     slug: 'owner/repo',
   });
 });
