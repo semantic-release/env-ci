62c7b75e2cace7550a3c486d43ba5a1a8aefef68
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-03-30 18:26:54 -0400

    test: test order

--- test/bamboo.test.js
+++ test/bamboo.test.js
@@ -1,15 +1,15 @@
 import test from 'ava';
 import bamboo from '../lib/bamboo';
 
+/* eslint-disable camelcase */
+
 test('Push', t => {
-	/* eslint-disable camelcase */
 	process.env.bamboo_buildKey = '1234';
 	process.env.bamboo_planRepository_1_revision = '5678';
 	process.env.bamboo_buildNumber = '91011';
 	process.env.bamboo_planRepository_1_branchName = 'master';
 	process.env.bamboo_build_working_directory = '/some/working/dir';
 	process.env.bamboo_buildResultsUrl = 'https://server.com/buildresult';
-	/* eslint-enable camelcase */
 
 	t.deepEqual(bamboo.configuration(), {
 		service: 'bamboo',
