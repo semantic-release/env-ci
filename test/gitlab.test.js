7684bc899f835515d2788bd87e7a158a60b23bda
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2018-04-20 16:34:18 +0530

    Used correct variable substitution to retrieve value from GitLab environment variables.

--- test/gitlab.test.js
+++ test/gitlab.test.js
@@ -4,24 +4,22 @@ import gitlab from '../lib/gitlab';
 test('Push', t => {
 	process.env.GITLAB_CI = 'true';
 	process.env.CI_COMMIT_SHA = '5678';
-	process.env.CI_JOB_NAME = 'job_name';
-	process.env.CI_JOB_STAGE = '1234';
 	process.env.CI_PIPELINE_ID = '91011';
 	process.env.CI_JOB_ID = '1213';
 	process.env.CI_PROJECT_URL = 'https://gitlab.com/owner/repo';
 	process.env.CI_COMMIT_REF_NAME = 'master';
-	process.env.CI_PROJECT_PATH = 'owner/repo';
+	process.env.CI_PROJECT_PATH_SLUG = 'owner/repo';
 	process.env.CI_PROJECT_DIR = '/';
 
 	t.deepEqual(gitlab.configuration(), {
 		name: 'GitLab CI/CD',
 		service: 'gitlab',
 		commit: '5678',
-		build: 'job_name',
+		build: '91011',
 		buildUrl: 'https://gitlab.com/owner/repo/pipelines/91011',
 		branch: 'master',
 		root: '/',
-		job: '1234',
+		job: '1213',
 		jobUrl: 'https://gitlab.com/owner/repo/-/jobs/1213',
 		slug: 'owner/repo',
 	});
