dc898fb6a68227daba739f1819bd3e6c874afd99
pvdlg <pierre.denis.vanduynslager@gmail.com>
2018-05-04 12:13:49 -0400

    feat(git): use remote branches only for local detached head
    If the local repository is not in a detached head state, use `git rev-parse --abbrev-ref HEAD` to determine the `branch` as it is more reliable.

--- test/git.test.js
+++ test/git.test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import git from '../lib/git';
-import {gitRepo, gitCommit, gitHead} from './helpers/git-utils';
+import {gitRepo, gitCommit, gitHead, gitCheckout} from './helpers/git-utils';
 
 // Save the current working diretory
 const cwd = process.cwd();
@@ -17,8 +17,15 @@ test.serial('Git repository', async t => {
 	t.deepEqual(git.configuration(), {commit, branch: 'master'});
 });
 
+test.serial('Git cloned repository', async t => {
+	await gitRepo(true);
+
+	t.deepEqual(git.configuration(), {commit: await gitHead(), branch: 'master'});
+});
+
 test.serial('Git repository with detached head', async t => {
 	await gitRepo(true);
+	await gitCheckout('HEAD~0', false);
 
 	t.deepEqual(git.configuration(), {commit: await gitHead(), branch: 'master'});
 });
