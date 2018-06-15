86d3b47029a3dc2eca8377dfe610b23e7439403b
Kyle Brown <Kyle_Brown@intuit.com>
2018-06-15 14:45:23 -0600

    fix: support circle 2.0 env vars

--- test/circleci.test.js
+++ test/circleci.test.js
@@ -26,7 +26,7 @@ test('Push', t => {
 	});
 });
 
-test('PR', t => {
+test('PR 1.0', t => {
 	process.env.CIRCLECI = 'true';
 	process.env.CIRCLE_BUILD_NUM = '1234';
 	process.env.CIRCLE_SHA1 = '5678';
@@ -35,6 +35,31 @@ test('PR', t => {
 	process.env.CIRCLE_PROJECT_USERNAME = 'owner';
 	process.env.CIRCLE_PROJECT_REPONAME = 'repo';
 	process.env.CI_PULL_REQUEST = 'uri/pr/10';
+
+	t.deepEqual(circle.configuration(), {
+		name: 'CircleCI',
+		service: 'circleci',
+		commit: '5678',
+		build: '1234',
+		buildUrl: 'https://server.com/buildresult',
+		job: '1234.1',
+		branch: 'pr_branch',
+		pr: '10',
+		isPr: true,
+		slug: 'owner/repo',
+	});
+});
+
+test('PR 2.0', t => {
+	process.env.CIRCLECI = 'true';
+	process.env.CIRCLE_BUILD_NUM = '1234';
+	process.env.CIRCLE_SHA1 = '5678';
+	process.env.CIRCLE_BRANCH = 'pr_branch';
+	process.env.CIRCLE_NODE_INDEX = '1';
+	process.env.CIRCLE_PROJECT_USERNAME = 'owner';
+	process.env.CIRCLE_PROJECT_REPONAME = 'repo';
+	process.env.CIRCLE_PULL_REQUEST = 'uri/pr/10';
+	delete process.env.CI_PULL_REQUEST;
 
 	t.deepEqual(circle.configuration(), {
 		name: 'CircleCI',
