75eeb030381635a2dd642d1ca7dc8af1f5709167
Kyle Brown <Kyle_Brown@intuit.com>
2018-06-15 15:02:08 -0600

    fix: circle 2.0 absent CIRCLE_PULL_REQUEST in favor of CIRCLE_PR_NUMBER

--- test/circleci.test.js
+++ test/circleci.test.js
@@ -74,3 +74,29 @@ test('PR 2.0', t => {
 		slug: 'owner/repo',
 	});
 });
+
+test('PR 2.0 without pull uri', t => {
+	process.env.CIRCLECI = 'true';
+	process.env.CIRCLE_BUILD_NUM = '1234';
+	process.env.CIRCLE_SHA1 = '5678';
+	process.env.CIRCLE_BRANCH = 'pr_branch';
+	process.env.CIRCLE_NODE_INDEX = '1';
+	process.env.CIRCLE_PROJECT_USERNAME = 'owner';
+	process.env.CIRCLE_PROJECT_REPONAME = 'repo';
+	process.env.CIRCLE_PR_NUMBER = '10';
+	delete process.env.CIRCLE_PULL_REQUEST;
+	delete process.env.CI_PULL_REQUEST;
+
+	t.deepEqual(circle.configuration(), {
+		name: 'CircleCI',
+		service: 'circleci',
+		commit: '5678',
+		build: '1234',
+		buildUrl: 'https://server.com/buildresult',
+		job: '1234.1',
+		branch: 'pr_branch',
+		pr: '10',
+		isPr: true,
+		slug: 'owner/repo',
+	});
+});
