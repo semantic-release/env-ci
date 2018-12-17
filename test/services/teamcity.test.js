7bb0c1f4c5120fdfbccc42179aba7c9c98216fce
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-12-17 17:34:16 -0500

    fix(teamcity): fallback for missing/erroneous properties file

--- test/services/teamcity.test.js
+++ test/services/teamcity.test.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import test from 'ava';
 import tempy from 'tempy';
 import teamcity from '../../services/teamcity';
+import {gitRepo} from '../helpers/git-utils';
 
 const env = {
 	TEAMCITY_VERSION: '2017.1.2 (build 46812)',
@@ -22,18 +23,6 @@ test('Push - with build properties file', t => {
 		build: '91011',
 		branch: 'master',
 		root: '/',
-		slug: 'owner/repo',
-	});
-});
-
-test('Push - without build properties file', t => {
-	t.deepEqual(teamcity.configuration({env}), {
-		name: 'TeamCity',
-		service: 'teamcity',
-		commit: '5678',
-		build: '91011',
-		branch: undefined,
-		root: undefined,
 		slug: 'owner/repo',
 	});
 });
@@ -78,4 +67,53 @@ test('Push - prioritize build properties file values', t => {
 		root: '/',
 		slug: 'owner/repo',
 	});
+});
+
+test('Push - without build properties file', async t => {
+	const {cwd} = await gitRepo(true);
+
+	t.deepEqual(teamcity.configuration({env, cwd}), {
+		name: 'TeamCity',
+		service: 'teamcity',
+		commit: '5678',
+		build: '91011',
+		branch: 'master',
+		root: undefined,
+		slug: 'owner/repo',
+	});
+});
+
+test('Push - with build and missing config properties files', async t => {
+	const {cwd} = await gitRepo(true);
+
+	const buildFile = tempy.file({extension: 'properties'});
+	const buildProperties = ['teamcity.build.branch=master', 'teamcity.configuration.properties.file=/tmp/null'];
+	fs.writeFileSync(buildFile, buildProperties.join('\n') + '\n');
+
+	t.deepEqual(teamcity.configuration({env: Object.assign({}, env, {TEAMCITY_BUILD_PROPERTIES_FILE: buildFile}), cwd}), {
+		name: 'TeamCity',
+		service: 'teamcity',
+		commit: '5678',
+		build: '91011',
+		branch: 'master',
+		root: undefined,
+		slug: 'owner/repo',
+	});
+});
+
+test('Push - with missing build properties files', async t => {
+	const {cwd} = await gitRepo(true);
+
+	t.deepEqual(
+		teamcity.configuration({env: Object.assign({}, env, {TEAMCITY_BUILD_PROPERTIES_FILE: '/tmp/null'}), cwd}),
+		{
+			name: 'TeamCity',
+			service: 'teamcity',
+			commit: '5678',
+			build: '91011',
+			branch: 'master',
+			root: undefined,
+			slug: 'owner/repo',
+		}
+	);
 });
