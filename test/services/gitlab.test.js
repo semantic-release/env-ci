9dbcddf23926ea6b631734ba5d2737d9802b671a
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2019-10-08 16:35:50 -0400

    feat: add support for `isPr`, `pr` and `prBranch` for GitLab CI/CD

--- test/services/gitlab.test.js
+++ test/services/gitlab.test.js
@@ -25,6 +25,38 @@ test('Push', t => {
 		root: '/',
 		job: '1213',
 		jobUrl: 'https://gitlab.com/owner/repo/-/jobs/1213',
+		pr: undefined,
+		isPr: false,
+		prBranch: undefined,
 		slug: 'owner/repo',
 	});
+});
+
+test('PR', t => {
+	t.deepEqual(
+		gitlab.configuration({
+			env: {
+				...env,
+				CI_MERGE_REQUEST_ID: '10',
+				CI_MERGE_REQUEST_TARGET_BRANCH_NAME: 'master',
+				CI_MERGE_REQUEST_SOURCE_BRANCH_NAME: 'pr-branch',
+			},
+		}),
+		{
+			name: 'GitLab CI/CD',
+			service: 'gitlab',
+			commit: '5678',
+			tag: 'tag_name',
+			build: '91011',
+			buildUrl: 'https://gitlab.com/owner/repo/pipelines/91011',
+			branch: 'master',
+			root: '/',
+			job: '1213',
+			jobUrl: 'https://gitlab.com/owner/repo/-/jobs/1213',
+			pr: '10',
+			isPr: true,
+			prBranch: 'pr-branch',
+			slug: 'owner/repo',
+		}
+	);
 });
