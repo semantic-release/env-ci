8982e8b1a388809137e32595c54c0d3bc2c05818
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2019-10-08 17:14:31 -0400

    feat: add support for Scrutinizer

---
+++ test/services/scrutinizer.test.js
@@ -0,0 +1,40 @@
+import test from 'ava';
+import scrutinizer from '../../services/scrutinizer';
+
+const env = {
+	SCRUTINIZER: 'true',
+	SCRUTINIZER_SHA1: '5678',
+	SCRUTINIZER_INSPECTION_UUID: '91011',
+	SCRUTINIZER_BRANCH: 'master',
+};
+
+test('Push', t => {
+	t.deepEqual(scrutinizer.configuration({env}), {
+		name: 'Scrutinizer',
+		service: 'scrutinizer',
+		commit: '5678',
+		build: '91011',
+		branch: 'master',
+		pr: undefined,
+		isPr: false,
+		prBranch: undefined,
+	});
+});
+
+test('PR', t => {
+	t.deepEqual(
+		scrutinizer.configuration({
+			env: {...env, SCRUTINIZER_PR_NUMBER: '10', SCRUTINIZER_PR_SOURCE_BRANCH: 'pr-branch'},
+		}),
+		{
+			name: 'Scrutinizer',
+			service: 'scrutinizer',
+			commit: '5678',
+			build: '91011',
+			branch: 'master',
+			pr: '10',
+			isPr: true,
+			prBranch: 'pr-branch',
+		}
+	);
+});
