dacaeb06f56462f93aae76eb6e7a4653441a2172
bijenkorf-james-wakefield <james.wakefield@debijenkorf.nl>
2019-10-09 09:54:36 +0200

    fix(Cloudbuild): Rename substitution variables
    Rame substitution variables removing the $ prefix. Once they have been added to env in build config
    file they can be accessed without prefix.

--- services/cloudbuild.js
+++ services/cloudbuild.js
@@ -2,19 +2,19 @@
 
 module.exports = {
 	detect({env}) {
-		return Boolean(env.$BUILD_ID);
+		return Boolean(env.BUILD_ID);
 	},
 	configuration({env}) {
-		const buildUrl = `https://console.cloud.google.com/cloud-build/builds/${env.$BUILD_ID}`;
+		const buildUrl = `https://console.cloud.google.com/cloud-build/builds/${env.BUILD_ID}`;
 		return {
 			name: 'Google Cloud Build',
 			service: 'cloudbuild',
-			branch: env.$BRANCH_NAME,
-			commit: env.$COMMIT_SHA,
-			tag: env.$TAG_NAME,
-			build: env.$BUILD_ID,
+			branch: env.BRANCH_NAME,
+			commit: env.COMMIT_SHA,
+			tag: env.TAG_NAME,
+			build: env.BUILD_ID,
 			buildUrl,
-			slug: env.$REPO_NAME,
+			slug: env.REPO_NAME,
 		};
 	},
 };
