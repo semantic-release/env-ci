27e9fc361e57734f9d7d098fcebdad8864ae6b59
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2020-03-09 11:49:45 -0400

    fix(vsts): use `BUILD_SOURCEBRANCH` instead of `BUILD_SOURCEBRANCHNAME`

--- test/services/vsts.test.js
+++ test/services/vsts.test.js
@@ -5,12 +5,26 @@ const env = {
 	BUILD_BUILDURI: 'https://fabrikamfiber.visualstudio.com/_git/Scripts',
 	BUILD_SOURCEVERSION: '5678',
 	BUILD_BUILDNUMBER: '1234',
-	BUILD_SOURCEBRANCHNAME: 'master',
+	BUILD_SOURCEBRANCH: 'master',
 	BUILD_REPOSITORY_LOCALPATH: '/',
 };
 
 test('Push', t => {
 	t.deepEqual(vsts.configuration({env}), {
+		name: 'Visual Studio Team Services',
+		service: 'vsts',
+		commit: '5678',
+		build: '1234',
+		branch: 'master',
+		pr: undefined,
+		isPr: false,
+		prBranch: undefined,
+		root: '/',
+	});
+});
+
+test('Push - with long branch name', t => {
+	t.deepEqual(vsts.configuration({env: {...env, BUILD_SOURCEBRANCH: 'refs/heads/master'}}), {
 		name: 'Visual Studio Team Services',
 		service: 'vsts',
 		commit: '5678',
