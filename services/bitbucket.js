c52c800b1c588aa98ad588d092cf7589e43e48f5
Gert Hengeveld <info@ghengeveld.nl>
2021-01-29 12:50:30 +0100

    Fix `slug` to follow `<owner_name>/<repo_name>` format

--- services/bitbucket.js
+++ services/bitbucket.js
@@ -13,7 +13,7 @@ module.exports = {
       build: env.BITBUCKET_BUILD_NUMBER,
       buildUrl: `https://bitbucket.org/${env.BITBUCKET_REPO_SLUG}/addon/pipelines/home#!/results/${env.BITBUCKET_BUILD_NUMBER}`,
       branch: env.BITBUCKET_BRANCH,
-      slug: env.BITBUCKET_REPO_SLUG,
+      slug: env.BITBUCKET_REPO_FULL_NAME,
       root: env.BITBUCKET_CLONE_DIR,
     };
   },
