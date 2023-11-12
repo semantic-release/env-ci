3fa0c8a7d5d078be690e23e3c5d9a9930dc366e2
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2023-11-12 11:41:45 +0100

    Update woodpecker env vars

--- services/woodpecker.js
+++ services/woodpecker.js
@@ -5,18 +5,18 @@ export default {
     return env.CI && env.CI === "woodpecker";
   },
   configuration({ env }) {
-    const isPr = env.CI_BUILD_EVENT === "pull_request";
+    const isPr = env.CI_PIPELINE_EVENT === "pull_request";
 
     return {
       name: "Woodpecker CI",
       service: "woodpecker",
       commit: env.CI_COMMIT_SHA,
       tag: env.CI_COMMIT_TAG,
-      build: env.CI_BUILD_NUMBER,
-      buildUrl: env.CI_BUILD_LINK,
+      build: env.CI_PIPELINE_NUMBER,
+      buildUrl: env.CI_PIPELINE_URL,
       branch: isPr ? env.CI_COMMIT_TARGET_BRANCH : env.CI_COMMIT_BRANCH,
-      job: env.CI_JOB_NUMBER,
-      jobUrl: env.CI_BUILD_LINK,
+      job: env.CI_STEP_NUMBER,
+      jobUrl: env.CI_STEP_URL,
       pr: env.CI_COMMIT_PULL_REQUEST,
       isPr,
       prBranch: isPr ? env.CI_COMMIT_SOURCE_BRANCH : undefined,
