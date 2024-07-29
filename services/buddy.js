033369c66f64fb6e9d7d169128f23fd3badbaacd
Lucas Czulak <lucas.czulak@buddy.works>
2024-07-29 12:48:18 +0200

    update buddy.js

--- services/buddy.js
+++ services/buddy.js
@@ -9,6 +9,7 @@ export default {
   configuration({ env }) {
     const pr = prNumber(env.BUDDY_EXECUTION_PULL_REQUEST_ID);
     const isPr = Boolean(pr);
+    const branch = isPr ? env.BUDDY_EXECUTION_PULL_REQUEST_HEAD_BRANCH : env.BUDDY_EXECUTION_BRANCH;
 
     return {
       name: "Buddy",
@@ -17,7 +18,8 @@ export default {
       tag: env.BUDDY_EXECUTION_TAG,
       build: env.BUDDY_EXECUTION_ID,
       buildUrl: env.BUDDY_EXECUTION_URL,
-      branch: isPr ? env.BUDDY_EXECUTION_PULL_REQUEST_HEAD_BRANCH : env.BUDDY_EXECUTION_BRANCH,
+      branch,
+      prBranch: isPr ? branch : undefined,
       pr,
       isPr,
       slug: env.BUDDY_REPO_SLUG,
